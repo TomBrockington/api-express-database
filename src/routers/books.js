3ac6a1dce8ae1a5a584f06031900963d6d600c75
Thomas Brockington <xtombrock1989@gmail.com>
2022-08-23 17:05:40 +0100

    Stuck on put

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,71 +1,117 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../../db");
 
-router.get('/', async (req, res) => {
-    console.log("loading all books");
-    const params = []
-    
-    let sqlQuery = 'SELECT * FROM books'
+// Get all books and books by type
+router.get("/", async (req, res) => {
+  console.log("loading all books");
+  const params = [];
 
-    if (req.query.type) {
-        sqlQuery += ` WHERE type = $1`
-        params.push(req.query.type)
-        console.log('params', params);
+  let sqlQuery = "SELECT * FROM books";
 
-    }
+  if (req.query.type) {
+    sqlQuery += ` WHERE type = $1`;
+    params.push(req.query.type);
+    console.log("params", params);
+  }
 
-    if (req.query.topic) {
-        sqlQuery += ` WHERE topic = $1`
-        params.push(req.query.topic)
-        console.log('params', params);
-    }
+  if (req.query.topic) {
+    sqlQuery += ` WHERE topic = $1`;
+    params.push(req.query.topic);
+    console.log("params", params);
+  }
 
-    const qResult = await db.query(sqlQuery, params)
+  const qResult = await db.query(sqlQuery, params);
 
-    res.status(200).json({
-        books: qResult.rows
-    })
-})
-
-// Create a book
-router.post("/", (req, res) => {
-    console.log("adding a book")
-
-    const newBook = req.body
-    newBook.id = books.length + 1
-    books.push(newBook)
-
-    res.status(201).json({
-        newBook
-    })
-})
+  res.status(200).json({
+    books: qResult.rows,
+  });
+});
 
 // Get book by ID
-router.get('/:id', async (req, res) => { 
-    console.log("loading all books");
-    const params = []
+router.get("/:id", async (req, res) => {
+  console.log("loading all books");
 
-    let sqlQuery = ' SELECT * FROM books WHERE '
+  const params = [];
+  let sqlQuery = " SELECT * FROM books WHERE ";
 
-    console.log('sql', sqlQuery);
-    console.log('params', params);
-    console.log('req. params', req.params);
+  if (!req.params.id) {
+    return res.status(404).json({
+      error: "Not Found",
+    });
+  } else {
+    sqlQuery += ` id = $1`;
+    params.push(req.params.id);
+    console.log("params.id", req.params.id);
+  }
+
+  const qResult = await db.query(sqlQuery, params);
+
+  res.status(200).json({
+    book: qResult.rows[0],
+  });
+});
+
+// Create a book
+router.post("/", async (req, res) => {
+  console.log("adding a book");
+  const params = [];
+
+  let sqlQuery = `
+        INSERT INTO books
+        (title, type, author, topic, publicationdate, pages)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING * 
+    `;
+
+  const newBook = req.body;
+
+  params.push(
+    newBook.title,
+    newBook.type,
+    newBook.author,
+    newBook.topic,
+    newBook.publicationdate,
+    newBook.pages
+  );
+
+  const qResult = await db.query(sqlQuery, params);
+
+  res.status(201).json({
+    book: qResult.rows[0],
+  });
+});
+
+// Update a book by ID
+router.put("/:id", async (req, res) => {
+  console.log("updating book");
+
+  const params = [];
+
+  let sqlQuery = `
+        UPDATE books
+        SET  ($1, $2)
+        WHERE ($3)
+        RETURNING *
+    `
+    const updateToMake = req.body
 
     if (req.params.id) {
-        sqlQuery += ` id = $1`
-        params.push(req.params.id)
-        console.log('params.id', req.params.id);
+        sqlQuery += ` id = $1`;
+        params.push(req.params.id);
+        console.log("params.id", req.params.id);
+
+        if (req.body) {
+            console.log('req.body', req.body);
+            console.log('url', sqlQuery);
+        }
     }
 
-    const qResult = await db.query(sqlQuery, params)
+    const qResult = await db.query(sqlQuery, params, updateToMake);
 
-    console.log('p2', params);
-    console.log('sql2', sqlQuery);
+    res.status(201).json({
+        book: qResult
+    });
+});
 
-    res.status(200).json({
-        books: qResult
-    }) 
-})
-
-module.exports = router
+module.exports = router;
